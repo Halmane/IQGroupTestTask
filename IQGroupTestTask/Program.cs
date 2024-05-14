e596ed238c4d35c5907301e01e943a56d024f0be
Halmane <durmanovova@gmail.com>
2024-05-15 00:42:00 +0400

    Fixed builder.Services from Program

--- IQGroupTestTask/Program.cs
+++ IQGroupTestTask/Program.cs
@@ -1,47 +1,49 @@
+using System;
 using IQGroupTestTask;
 using IQGroupTestTask.Components;
-using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddRazorComponents().AddInteractiveServerComponents();
-var mongoClientLink = builder.Configuration.GetSection("MongoClientLink").Value;
-
-if (string.IsNullOrEmpty(mongoClientLink))
+try
 {
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    builder.Services.AddRazorComponents().AddInteractiveServerComponents();
+    var mongoClientLink = builder.Configuration.GetSection("MongoClientLink").Value;
+
     builder
         .Services.AddLogging(builder => builder.AddConsole())
-        .AddSingleton(new MongoClient())
         .AddSingleton<MongoDatabaseUserService>();
+
+    ArgumentNullException.ThrowIfNullOrEmpty(mongoClientLink);
+
+    builder.Services.AddSingleton(new MongoClient(mongoClientLink));
+
+    var app = builder.Build();
+
+    var mongoClient = app.Services.GetService<MongoClient>();
+    var mongoDBUserService = app.Services.GetService<MongoDatabaseUserService>();
+
+    // Configure the HTTP request pipeline.
+    if (!app.Environment.IsDevelopment())
+    {
+        app.UseExceptionHandler("/Error", createScopeForErrors: true);
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseStaticFiles();
+    app.UseAntiforgery();
+
+    app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
+
+    app.Run();
 }
-else
+catch (Exception ex)
 {
-    builder
-        .Services.AddLogging(builder => builder.AddConsole())
-        .AddSingleton(new MongoClient(mongoClientLink))
-        .AddSingleton<MongoDatabaseUserService>();
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Press any key to exit app...");
+    Console.ReadKey();
 }
-
-var app = builder.Build();
-
-var mongoClient = app.Services.GetService<MongoClient>();
-var mongoDBUserService = app.Services.GetService<MongoDatabaseUserService>();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-app.UseAntiforgery();
-
-app.MapRazorComponents<App>().AddInteractiveServerRenderMode();
-
-app.Run();
